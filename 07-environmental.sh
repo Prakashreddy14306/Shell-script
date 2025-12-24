e4ca599705b17f989e91803dd12f997f17ef5911
prakash <prakash@gmail.com>
2025-12-24 10:51:38 +0530

    first

--- 07-environmental.sh
+++ 07-environmental.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-read -p "enter the variable:-" var
+# read -p "enter the variable:-" var
 
-echo "environmental:${!var}"
+# echo "environmental:${!var}"
+echo "Logged in as: $USER"
+echo "Home directory: $HOME"
+echo "Current shell: $SHELL"
+echo "Operating System: $OSTYPE"
+echo "Path: $PATH"
+echo "Current Directory: $PWD"
+echo "Terminal Type: $TERM"
