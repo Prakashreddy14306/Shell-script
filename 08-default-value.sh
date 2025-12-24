3dd231c0166288b31f41d16faa117e2ac3adf0ce
prakash <prakash@gmail.com>
2025-12-24 11:04:42 +0530

    first

--- 08-default-value.sh
+++ 08-default-value.sh
@@ -2,5 +2,6 @@
 colour=${colour:-"red"}
 echo "The colour is: $colour"
 
-ls | grep ".sh" | wc -sdl 2>&1 output.txt
+ls | grep ".sh" | wc -l 2>&1 output.txt
+
 echo "Number of .sh files: $(cat output.txt)"
