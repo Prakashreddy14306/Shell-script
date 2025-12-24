4a5c79a4391be30de531632ed86c5d14c562afe8
prakash <prakash@gmail.com>
2025-12-24 11:27:24 +0530

    first

--- 09-loops.sh
+++ 09-loops.sh
@@ -2,9 +2,11 @@
 #!/bin/bash
 for i in {1..5}; do
     if [ $i -eq 4 ]; then
+        echo "Breaking at $i"
         break
     fi
     if [ $((i % 2)) -eq 0 ]; then
+        echo "Even: $i"
         continue
     fi
     echo "Odd: $i"
