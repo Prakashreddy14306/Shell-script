9fa4369afca6fe5f4dd393f6ae2444b0993400e9
prakash <prakash@gmail.com>
2025-12-24 10:54:56 +0530

    first

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-PERSON1=$1
-PERSON2=$2
+# PERSON1=$1
+# PERSON2=$2
 
-echo "$PERSON1:: Hi $PERSON2"
-echo "${PERSON2}:: Hello $PERSON1"
-echo "$PERSON1:: How are you doing?"
-echo "$PERSON2:: I am good. How are you?"
+# echo "$PERSON1:: Hi $PERSON2"
+# echo "${PERSON2}:: Hello $PERSON1"
+# echo "$PERSON1:: How are you doing?"
+# echo "$PERSON2:: I am good. How are you?"
+
+#!/bin/bash
+age=25
+new_age=$((age+5))
+echo "In 5 years, you'll be $new_age"
