49dc03cb2a5f038ab2cd2748c3ab6a9900c84d22
Prakash <Prakash@gmail.com>
2026-02-19 15:35:57 +0530

    shell

---
+++ new-shell/04-check-file.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+
+
+
+
+check_file() {
+    if [ -f "$1" ]; then
+        echo "File '$1' exists."
+    else
+        echo "File '$1' does not exist."
+    fi
+}
+
+check_file "$1"
