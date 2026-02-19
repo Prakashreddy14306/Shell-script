7cd66d1c6d5bd536f546773f17d12a5a1c516ea2
Prakash <Prakash@gmail.com>
2026-02-19 15:30:57 +0530

    shell

--- new-shell/03.sh
+++ new-shell/03.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-echo "First arg: $1"
-echo "Second arg: $2"
-echo "All args: $@"
-echo "Total args: $#"
+# echo "First arg: $1"
+# echo "Second arg: $2"
+# echo "All args: $@"
+# echo "Total args: $#"
+
+
+read -p "Enter your name: " name
+read -p "region: " region
+read -p "instance type: " instance_type
+
+echo "name is : $name and region is : $region and instance type is : $instance_type"
